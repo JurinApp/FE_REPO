ecd67bbe043a1c269481ce634dc961614711e82f
Cho-SeongJu <tjdwn9753@gmail.com>
2023-12-26 22:12:40 +0900

    ✨ Feat : useInput custom hook 추가
    - useInput custom hook 추가
    - 공통으로 사용
    - debounce 방식 도입

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,14 @@
+import _ from "lodash";
+import { useState } from "react";
+
+const useInput = (initValue: any) => {
+	const [text, setText] = useState<any>(initValue);
+
+	const onChangeTextHandler = _.debounce((e) => {
+		setText(e.target.value);
+	}, 1000);
+
+	return { text, onChangeTextHandler };
+};
+
+export default useInput;
