f4fb0b35b96f3da96b22adb9a7da26158f1cd5db
Cho-SeongJu <tjdwn9753@gmail.com>
2023-12-27 00:17:22 +0900

    🎨 Design : 공통으로 사용할 색상, font 설정 적용
    - 공통으로 사용할 색상, font 설정 적용
    - 폰트 추가 (타이틀/제목 폰트, 본문 폰트)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				tekhelet: "rgba(61, 52, 139, 1)",
+				"disabled-tekhelet": "rgba(61, 52, 139, 0.3)",
+				iris: "rgba(90, 86, 188, 1)",
+				"medium-slate-blue": "rgba(118, 120, 237, 1)",
+				tangerine: "rgba(241, 135, 1, 1)",
+				danger: "rgba(241, 50, 50, 1)",
+				persimmon: "rgba(243, 91, 4, 1)",
+				"selective-yellow": "rgba(247, 184, 1, 1)",
+			},
+			fontFamily: {
+				HakgyoansimUndongjangL: ["HakgyoansimUndongjangL"],
+			},
+		},
 	},
 	plugins: [],
 };
