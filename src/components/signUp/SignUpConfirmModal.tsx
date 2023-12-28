c79900649beeb955ae605b49452baac4ab3f1ffa
Cho-SeongJu <tjdwn9753@gmail.com>
2023-12-28 10:13:56 +0900

    ✨ Feat : 회원가입 확인 모달 외부 영역 클릭 시 모달 닫기 기능 추가
    - 회원가입 확인 모달 외부 영역 클릭 시 모달 닫기 기능 추가

--- src/components/signUp/SignUpConfirmModal.tsx
+++ src/components/signUp/SignUpConfirmModal.tsx
@@ -1,8 +1,10 @@
 import { openConfirmModalState } from "@/states/openConfirmModal";
+import { useEffect, useRef } from "react";
 import { useSetRecoilState } from "recoil";
 
 const SignUpConfirmModal = () => {
 	const setIsOpenConfirmModal = useSetRecoilState(openConfirmModalState);
+	const modalRef = useRef<HTMLDivElement>(null);
 
 	const closeBtnModalHandler = () => {
 		setIsOpenConfirmModal(false);
@@ -12,26 +14,43 @@ const SignUpConfirmModal = () => {
 		setIsOpenConfirmModal(false);
 	};
 
+	useEffect(() => {
+		const outSideClickHandler = (e: Event) => {
+			if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+				setIsOpenConfirmModal(false);
+			}
+		};
+
+		document.addEventListener("mousedown", outSideClickHandler);
+
+		return () => {
+			document.removeEventListener("mousedown", outSideClickHandler);
+		};
+	}, [modalRef]);
+
 	return (
-		<div className="bg-black-800 fixed top-0 flex h-full w-full items-center justify-center">
-			<div className="flex h-[12rem] w-[333px] flex-col rounded bg-white">
+		<div className="fixed top-0 flex h-full w-full items-center justify-center bg-black-800">
+			<div
+				ref={modalRef}
+				className="flex h-[12rem] w-[333px] flex-col rounded bg-white"
+			>
 				<div className="flex grow items-center justify-center">
 					<p className="my-auto">
-						<span className="text-tekhelet font-medium">선생님</span>으로
+						<span className="font-medium text-tekhelet">선생님</span>으로
 						가입하시겠습니까?
 					</p>
 				</div>
 				<div className="flex">
 					<button
 						type="button"
-						className="text-black-800 bg-btn-cancel-tekhelet h-[3.75rem] grow rounded-bl"
+						className="h-[3.75rem] grow rounded-bl bg-btn-cancel-tekhelet text-black-800"
 						onClick={closeBtnModalHandler}
 					>
 						취소
 					</button>
 					<button
 						type="button"
-						className="bg-medium-slate-blue h-[3.75rem] grow rounded-br font-bold text-white"
+						className="h-[3.75rem] grow rounded-br bg-medium-slate-blue font-bold text-white"
 						onClick={confirmBtnModalHandler}
 					>
 						확인
