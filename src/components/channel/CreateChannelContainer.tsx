cc0ff1a2e2d355c59147853570b12859855def7b
khakisage <jongjin201@gmail.com>
2024-01-05 16:39:49 +0900

    🎨 Design : px 값 rem 으로 변경
    [description]
    
    [issue-number]
    #2

--- src/components/channel/CreateChannelContainer.tsx
+++ src/components/channel/CreateChannelContainer.tsx
@@ -2,7 +2,7 @@ export const CreateChannelContainer = () => {
 	return (
 		<div className="mx-auto h-[50.312rem] w-[393px] bg-[#ffffff]">
 			<div className="flex h-[50.312rem] flex-col justify-end gap-4">
-				<h1 className=" ml-4 text-[26px] font-bold">채널 생성</h1>
+				<h1 className=" ml-4 text-[1.625rem] font-bold">채널 생성</h1>
 				<div
 					className="mx-4 mt-4 flex h-auto w-[361px] flex-col gap-4 rounded"
 					id="channelSection"
@@ -16,7 +16,7 @@ export const CreateChannelContainer = () => {
 						placeholder="채널 이름"
 						className="border-b pb-2 text-base placeholder-gray-300 focus:border-b focus:border-gray-700 focus:outline-none"
 					/>
-					<p className="mx-auto mt-16 text-[14px] font-normal text-[#3d348b]">
+					<p className="mx-auto mt-16 text-sm font-normal text-[#3d348b]">
 						채널 생성 시 코드가 랜덤으로 부여됩니다.
 					</p>
 				</div>
--- src/components/modifyUserinfo/ConfirmModal.tsx
+++ src/components/modifyUserinfo/ConfirmModal.tsx
@@ -9,16 +9,19 @@ export const ConfirmModal = (props: TConfirmModalProps) => {
 
 	return (
 		<>
-			<div className="fixed left-1/2 top-1/2 h-[852px] w-[393px] -translate-x-1/2 -translate-y-1/2 transform bg-black opacity-50"></div>
-			<div className="fixed left-1/2 top-1/2 flex h-[179px] w-[333px] -translate-x-1/2 -translate-y-2/3 transform flex-col">
-				<div className="bg-opacity-2 flex h-[119px] items-center justify-center bg-[#ffffff]">
-					<p className="font-[500] text-[#000000]">수정하시겠습니까?</p>
+			<div
+				className="fixed left-1/2 top-0 h-[100vh] w-[24.563rem] -translate-x-1/2  transform bg-black-800"
+				onClick={onCancel}
+			></div>
+			<div className="fixed left-1/2 top-1/2 flex h-[17.5rem] w-[333px] -translate-x-1/2 -translate-y-1/3 transform flex-col">
+				<div className="bg-opacity-2 flex h-[7.438rem] items-center justify-center bg-[#ffffff]">
+					<p className="font-medium text-[#000000]">수정하시겠습니까?</p>
 				</div>
-				<div className="flex h-[60px] flex-row">
+				<div className="flex h-[3.75rem] flex-row">
 					<button className="w-1/2 bg-gray-300" onClick={onCancel}>
 						취소
 					</button>
-					<button className="w-1/2 bg-[#3d348b] text-[#ffffff]">확인</button>
+					<button className="w-1/2 bg-tekhelet text-[#ffffff]">확인</button>
 				</div>
 			</div>
 		</>
