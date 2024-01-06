67786a7e85e9937bf13c03516408a92c94680192
khakisage <jongjin201@gmail.com>
2024-01-06 21:51:10 +0900

    🎨 Design : tailwind config color 추가
    [description]
    
    [issue-number]
    #16

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ export default {
 			colors: {
 				tekhelet: "rgba(61, 52, 139, 1)",
 				"btn-cancel-tekhelet": "rgba(61, 52, 139, 0.05)",
+				"btn-cancel": "rgba(245,245,249,1)",
 				"disabled-tekhelet": "rgba(61, 52, 139, 0.3)",
 				iris: "rgba(90, 86, 188, 1)",
 				"medium-slate-blue": "rgba(118, 120, 237, 1)",
 				tangerine: "rgba(241, 135, 1, 1)",
 				danger: "rgba(241, 50, 50, 1)",
+				"disabled-danger": "rgba(241,50,50,0.5)",
 				persimmon: "rgba(243, 91, 4, 1)",
 				"selective-yellow": "rgba(247, 184, 1, 1)",
 				"black-100": "rgba(0,0,0,0.1)",
