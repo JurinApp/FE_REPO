b449dff6eae5a1154548e6aec8b3e7043efb2781
Cho-SeongJu <tjdwn9753@gmail.com>
2024-01-16 21:14:59 +0900

    ✨ Feat : Header에 Link 추가 및 login 전역 상태 추가
    - Header에 Link 추가
    - login 전역 상태 추가

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,17 +1,21 @@
 import Logo from "@assets/svg/colorLogo.svg?react";
 import Setting from "@assets/svg/setting.svg?react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+	const location = useLocation();
+	// TODO : 권한 별로 나누어 경로 지정해줄 예정
+	const path = location.pathname === "/signUp" ? "/login" : "";
+
 	return (
 		<header className="sticky top-0 z-[99] mx-auto h-[2.938rem] w-full border-b border-black-200 bg-white sm:w-[24.563rem]">
 			<figure className="flex h-full items-center justify-between px-4">
-				<div className="flex items-center">
+				<Link to={path} className="flex items-center">
 					<Logo className="h-6 w-5" />
 					<p className="ml-2 font-Title font-light text-iris">
 						주린이를 부탁해
 					</p>
-				</div>
+				</Link>
 				<div className="flex items-center justify-center">
 					<Link to="/setting">
 						<Setting />
