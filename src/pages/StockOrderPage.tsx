a3424c092486dd6624ae000d3a92a72ce8d35b18
khakisage <jongjin201@gmail.com>
2024-01-21 02:49:15 +0900

    🏗️ Structure : 주식 거래 페이지 삭제
    [description]
    - 컴포넌트 렌더링으로 변경
    - 기존 페이지 삭제
    
    [issue-number]
    #18

--- src/pages/StockOrderPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StockOrderPage = () => {
-	return <div>StockOrderPage</div>;
-};
-
-export default StockOrderPage;
--- src/pages/StockSellPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StockSellPage = () => {
-	return <div>StockSellPage</div>;
-};
-
-export default StockSellPage;
