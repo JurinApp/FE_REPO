ce71f9b9e4085a6edea1c8f25e53962ec7d22a19
khakisage <jongjin201@gmail.com>
2024-01-21 02:50:28 +0900

    ✨ Feat : stockHeader 기능 구현
    [description]
    - 뒤로가기 기능
    - 주식 이름 Props 추가
    
    [issue-number]
    #18

--- src/components/common/header/StockHeader.tsx
+++ src/components/common/header/StockHeader.tsx
@@ -1,16 +1,20 @@
 import Btn from "@assets/svg/btn_back.svg?react";
-const StockHeader = () => {
+import { useNavigate } from "react-router";
+type StockHeaderProps = {
+	name?: string;
+};
+const StockHeader = (props: StockHeaderProps) => {
+	const navigate = useNavigate();
+
+	const goBackHandler = () => {
+		navigate(-1);
+	};
 	return (
 		<header className="sticky top-0 z-[99] mx-auto h-[2.938rem] w-full  bg-white sm:w-[24.563rem]">
 			<figure className="flex h-full items-center justify-between px-4">
 				<div className="flex items-center">
-					<Btn width={20} height={24} />
-					<p className="ml-2 text-base font-bold text-black">
-						{
-							// 주식 이름
-						}
-						선생님의 몸무게
-					</p>
+					<Btn width={20} height={24} onClick={goBackHandler} />
+					<p className="ml-2 text-base font-bold text-black">{props.name}</p>
 				</div>
 			</figure>
 		</header>
