e74ceef8a46996aa13e375a06ab7a9441286957e
Cho-SeongJu <tjdwn9753@gmail.com>
2024-01-21 19:07:04 +0900

    💄 Design : spinner left값 수정

--- src/components/common/spinner/Spinner.tsx
+++ src/components/common/spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import { BeatLoader } from "react-spinners";
 
 const Spinner = () => {
 	return (
-		<div className="fixed top-0 z-[100] flex min-h-screen w-full items-center justify-center bg-[rgba(229,229,229,0.6)]">
+		<div className="fixed left-0 top-0 z-[100] flex min-h-screen w-full items-center justify-center bg-[rgba(229,229,229,0.6)]">
 			<BeatLoader color="#7678ED" />
 		</div>
 	);
