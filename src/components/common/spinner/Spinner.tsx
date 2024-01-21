cd4d56701ea93b888def0b0a505af2dfec975690
Cho-SeongJu <tjdwn9753@gmail.com>
2024-01-21 20:15:35 +0900

    ✨ Feat : Spinner scroll제어 코드 추가, login 에러 핸들링 수정 및 spinner 추가
    - Spinner가 mount 시점에 body scroll 안되게 수정
    - unmount 시점에는 scroll 되게 수정
    - login 에러 핸들링 수정
    - api response 대기 시 spinner 컴포넌트 출력되게 수정

--- src/components/common/spinner/Spinner.tsx
+++ src/components/common/spinner/Spinner.tsx
@@ -1,6 +1,19 @@
+import {
+	cancelLockBodyScroll,
+	lockBodyScroll,
+} from "@/utils/controlBodyScroll";
+import { useEffect } from "react";
 import { BeatLoader } from "react-spinners";
 
 const Spinner = () => {
+	useEffect(() => {
+		lockBodyScroll();
+
+		return () => {
+			cancelLockBodyScroll();
+		};
+	}, []);
+
 	return (
 		<div className="fixed left-0 top-0 z-[100] flex min-h-screen w-full items-center justify-center bg-[rgba(229,229,229,0.6)]">
 			<BeatLoader color="#7678ED" />
