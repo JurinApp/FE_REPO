a90f957963814b738ff11c331756173589c9dba2
khakisage <jongjin201@gmail.com>
2024-02-02 20:22:36 +0900

    ✨ Feat : 모달 바깥 영역 ref 추가
    [description]
    
    [issue-number]
    #42

--- src/components/student/studentItem/myItem/ItemUseModal.tsx
+++ src/components/student/studentItem/myItem/ItemUseModal.tsx
@@ -1,6 +1,7 @@
 import { IMyItem } from "./MyItemContainer";
 import { useRecoilState } from "recoil";
 import { itemUseModalState } from "@/states/confirmModalState";
+import { useEffect, useRef } from "react";
 type TItemBuyModalProps = {
 	readonly onConfirm: () => void;
 	readonly item: IMyItem;
@@ -10,7 +11,19 @@ const ItemUseModal = ({ onConfirm, item }: TItemBuyModalProps) => {
 	// const [itemQuantity, setItemQuantity] = useState(1);
 	const [isItemUseModalOpen, setIsItemUseModalOpen] =
 		useRecoilState(itemUseModalState);
+	const modalRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		const handleOutSideClick = (e: Event) => {
+			if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+				setIsItemUseModalOpen(false);
+			}
+		};
+		document.addEventListener("mousedown", handleOutSideClick);
 
+		return () => {
+			document.removeEventListener("mousedown", handleOutSideClick);
+		};
+	}, [modalRef]);
 	const handleModalClose = () => {
 		setIsItemUseModalOpen(false);
 	};
@@ -18,41 +31,45 @@ const ItemUseModal = ({ onConfirm, item }: TItemBuyModalProps) => {
 		<>
 			<div
 				className={`${
-					isItemUseModalOpen ? "fixed" : "hidden"
-				} left-1/2 top-1/2 flex h-[25.563rem] w-[20.813rem] -translate-x-1/2 -translate-y-1/2 transform flex-col`}
+					isItemUseModalOpen ? "flex" : "hidden"
+				} fixed top-0 z-[100] flex h-full w-full flex-col items-center justify-center bg-black-700`}
 			>
-				<div className="bg-opacity-2 flex h-[21.813rem] justify-center bg-[#ffffff]">
-					<div className="flex flex-col">
-						<div className="border-b-main-disabled mt-12 flex h-[5.063rem] w-[17.813rem] justify-center border-b">
-							<p className="text-lg font-medium">
-								<span className="font-bold text-tekhelet">{item.itemName}</span>
-								을
-								<br />
-								구매하시겠습니까?
-							</p>
-						</div>
-						<div className="flex w-[17.813rem] flex-col">
-							<div className="mt-6 flex w-[17.813rem] flex-row">
-								<p className="ml-[2.375rem] flex items-center text-base">
-									남은 수량
+				<div ref={modalRef}>
+					<div className="bg-opacity-2 flex h-[21.813rem] w-[20.813rem] justify-center bg-[#ffffff]">
+						<div className="flex flex-col">
+							<div className="border-b-main-disabled mt-12 flex h-[5.063rem] w-[17.813rem] justify-center border-b">
+								<p className="text-lg font-medium">
+									<span className="font-bold text-tekhelet">
+										{item.itemName}
+									</span>
+									을
+									<br />
+									구매하시겠습니까?
 								</p>
-								<p className="ml-[1.75rem] flex h-10 w-[7.375rem] items-center justify-end text-right text-base">
-									{item.quantity} 개
-								</p>
+							</div>
+							<div className="flex w-[17.813rem] flex-col">
+								<div className="mt-6 flex w-[17.813rem] flex-row">
+									<p className="ml-[2.375rem] flex items-center text-base">
+										남은 수량
+									</p>
+									<p className="ml-[1.75rem] flex h-10 w-[7.375rem] items-center justify-end text-right text-base">
+										{item.quantity} 개
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="flex h-[3.75rem] flex-row">
-					<button className="w-1/2 bg-btn-cancel" onClick={handleModalClose}>
-						취소
-					</button>
-					<button
-						className="w-1/2 bg-medium-slate-blue text-[#ffffff]"
-						onClick={onConfirm}
-					>
-						구매
-					</button>
+					<div className="flex h-[3.75rem] w-[20.813rem] flex-row">
+						<button className="w-1/2 bg-btn-cancel" onClick={handleModalClose}>
+							취소
+						</button>
+						<button
+							className="w-1/2 bg-medium-slate-blue text-[#ffffff]"
+							onClick={onConfirm}
+						>
+							구매
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
