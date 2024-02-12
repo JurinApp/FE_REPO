60246aab8ad4c2b36e15d0d0b1e29bde5be7a01b
Cho-SeongJu <tjdwn9753@gmail.com>
2024-02-13 04:06:45 +0900

    ✨ Feat : 아이템 영역 무한스크롤 기능 추가
    - 아이템 영역 무한스크롤 기능 추가
    - interface 추가

--- src/components/educator/item/inquiryItem/ItemList.tsx
+++ src/components/educator/item/inquiryItem/ItemList.tsx
@@ -1,24 +1,39 @@
-import { IItem } from "@/interface/item";
+import { IItem, IItemResponseData } from "@/interface/item";
 import ItemCard from "./ItemCard";
 import {
 	allCheckItemsState,
 	selectedItemState,
 } from "@/states/selectedItemState";
 import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
-import { useEffect } from "react";
+import { RefObject, useEffect } from "react";
+import IntersectSpinner from "@/components/common/spinner/IntersectSpinner";
+import ObserveTarget from "@/components/common/observer/ObserveTarget";
 
 interface IItemListProps {
-	readonly itemList: IItem[];
+	readonly responseData: IItemResponseData[];
+	readonly isFetching: boolean;
+	readonly observeTargetRef: RefObject<HTMLDivElement>;
 }
 
-const ItemList = ({ itemList }: IItemListProps) => {
+const ItemList = ({
+	responseData,
+	isFetching,
+	observeTargetRef,
+}: IItemListProps) => {
 	const selectedItems = useRecoilValue(selectedItemState);
 	const setIsAllCheck = useSetRecoilState(allCheckItemsState);
 	const resetSelectedItems = useResetRecoilState(selectedItemState);
-	const isExist = itemList.length === 0;
+
+	const flatMapItemList = responseData.flatMap((data) => {
+		return data.results.flatMap((item) => {
+			return item;
+		});
+	});
+
+	const isExist = flatMapItemList.length === 0;
 
 	useEffect(() => {
-		if (selectedItems.length === itemList.length) {
+		if (selectedItems.length === flatMapItemList.length) {
 			setIsAllCheck(true);
 		} else {
 			setIsAllCheck(false);
@@ -41,9 +56,16 @@ const ItemList = ({ itemList }: IItemListProps) => {
 				<p className="text-black-700">등록된 아이템이 없습니다.</p>
 			) : (
 				<div className="grid grid-cols-1 gap-1 sm:grid-cols-3 xs:grid-cols-2">
-					{itemList.map((item: IItem) => (
-						<ItemCard item={item} key={item.id} />
-					))}
+					<>
+						{flatMapItemList.map((item: IItem) => (
+							<ItemCard item={item} key={item.id} />
+						))}
+						{isFetching ? (
+							<IntersectSpinner />
+						) : (
+							<ObserveTarget observeTargetRef={observeTargetRef} />
+						)}
+					</>
 				</div>
 			)}
 		</div>
