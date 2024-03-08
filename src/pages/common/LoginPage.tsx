028728ef2aebf29d9e7cc59c1c2c7e310e435a2b
Cho-SeongJu <tjdwn9753@gmail.com>
2024-03-08 20:09:53 +0900

    ⚡️ Modify : 페이지 별 로고 클릭 시 이동 경로 수정
    - 페이지 별 로고 클릭 시 이동 경로 수정
    - 로그인 되어있는 상태에서 로그인 페이지 이동 시 마이페이지로 이동

--- src/pages/common/LoginPage.tsx
+++ src/pages/common/LoginPage.tsx
@@ -1,12 +1,20 @@
 import { headerMenuUseState } from "@/states/headerMenuUseState";
+import { userRoleState } from "@/states/userRoleState";
 import LoginContainer from "@components/login/LoginContainer";
 import { useLayoutEffect } from "react";
-import { useSetRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 const LoginPage = () => {
 	const setIsUseHeader = useSetRecoilState(headerMenuUseState);
+	const userRole = useRecoilValue(userRoleState);
+	const navigate = useNavigate();
 
 	useLayoutEffect(() => {
+		if (userRole !== "anonymous") {
+			navigate("/mypage");
+		}
+
 		setIsUseHeader({
 			isUseHeader: false,
 			isUseMenu: false,
